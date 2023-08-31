49132dd7bb4c91d5ba80ed835baa39e9e6dec335
kangyoungmin <kangym855@gmail.com>
2023-08-31 12:26:49 +0900

    0831_humberger

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,27 @@ $(function(){
         dots:true,
         arrows:false
     });
+    $('.ul-topping').slick({
+        infinite: true,
+        slidesToShow:3,
+        dots:false,
+        arrows: false
+    });
+
+    $('.down').on("click",function(){
+        let ct = $('.count-only').val();
+        if(ct > 1){
+            ct--;
+            $('.count-only').val(ct);
+        }
+    });
+
+    $('.up').on("click",function(){
+        let ct = $('.count-only').val();
+        ct++;
+        $('.count-only').val(ct);
+        
+    });
+
+    $('.')
 });  //jquery
